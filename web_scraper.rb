7c6754a8b00c976ffa036895d6c458aacce8d031
bklynate <nathanielcfa@gmail.com>
2016-02-06 14:26:08 -0500

    add commented code

--- web_scraper.rb
+++ web_scraper.rb
@@ -3,9 +3,13 @@ require 'nokogiri'
 require 'csv'
 require 'pry'
 
+# Url that is to be scraped
 url = "https://www.airbnb.com/s/Brooklyn--New-York--NY"
+
+# Parse the HTML with nokogiri
 data = Nokogiri::HTML(open(url))
 
+# Scrapes the listing names into arrays
 listing_names = []
 data.css('h3.h5.listing-name').each do |listing|
   listing = listing.text
@@ -25,9 +29,15 @@ data.css('div.text-muted.listing-location.text-truncate').each do |details|
   details = details.delete("\n")
   listing_details << details
 end
+
+# Had to strip the extra whitespace within the strings in the array
+# binding.pry <- uncomment if you dont understand
 listing_details = listing_details.join.split(" ").join(" ").split("s ")
 
+# Write the csv file with the scraped data
 CSV.open("airbnb_listings.csv","w") do |file|
+  file <<  ["Listing Names", "Listing Prices", "Listing Details"]
+
   listing_names.length.times do |i|
     file << [listing_names[i],listing_prices[i],listing_details[i]]
   end
